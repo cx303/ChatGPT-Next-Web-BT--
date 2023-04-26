2b25b907823dec21ec3b00012330c3997e58ac35
cx303 <98731303+cx303@users.noreply.github.com>
2023-04-26 19:16:13 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "BT-地表最强容灾云",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "BT-地表最强容灾云",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
